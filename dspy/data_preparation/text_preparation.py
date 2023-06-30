6cd1edc7efe4a2c2be98e381d9fbd6b4da1846e5
nachomondino1 <nachomondino1@gmail.com>
2023-06-30 12:17:20 -0300

    Ya no defino atributos. 1) Por el dataframe ya no es un atributo? Entre otras razones porque:  - los métodos están relacionados directamente con la responsabilidad principal de la clase y su resultado es parte integral de su funcionamiento  - el valor que el método produce no es una información interna o un detalle de implementación que no es relevante para el código externo que utiliza la clase  - gano flexibilidad para reutilizar un mismo objeto para multiples dataframes. 2) Por que la lista de columnas ya no es un atributo? Por las mismas razones. Defino las columnas a procesar fuera de la clase y lo paso como argumento de cada metodo.

--- dspy/data_preparation/text_preparation.py
+++ dspy/data_preparation/text_preparation.py
@@ -8,51 +8,49 @@ from nltk.stem import SnowballStemmer
 class TextPreparation:
     """Techniques to prepare text"""
 
-    def __init__(self, textos, l_col_to_except=None):
-        self.df = pd.DataFrame(textos)  # Convertir a DataFrame
-        self.l_col_to_except = l_col_to_except if l_col_to_except is not None else []
-        self.set_columns_to_process()
+    def __init__(self):  # no defino df como atributo puesto que es muy importante que el usuario reciba el df de cada funcion.
+        pass
 
-    def set_columns_to_process(self):
-        """Define las columnas a procesar"""
-        cols_object = self.df.select_dtypes(include='object').columns
-        cols_to_process = [col for col in cols_object if col not in self.l_col_to_except]
-        self.columns_to_process = cols_to_process
-
-    def to_lower(self):
+    def to_lower(self, df, columns):
         """Convierte a minúscula los textos"""
-        for col in self.columns_to_process:
-            self.df[col] = self.df[col].str.lower()
+        for col in columns:
+            df[col] = df[col].str.lower()
+        return df
 
-    def delete_accent(self):
+    def delete_accent(self, df, columns):
         """Remueve acentos de los textos"""
         d = {'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u'}
-        for col in self.columns_to_process:
-            self.df[col] = self.df[col].replace(d, regex=True)
+        for col in columns:
+            df[col] = df[col].replace(d, regex=True)
+        return df
 
-    def delete_special_characters(self):
+    def delete_special_characters(self, df, columns):
         """Remueve caracteres especiales de los textos"""
         d = {'ã': 'a', 'â': 'a', 'ä': 'a', 'ê': 'e', 'ë': 'e', 'î': 'i', 'ï': 'i', 'ô': 'o', 'ö': 'o', 'ø': 'o',
              'û': 'u', 'ü': 'u', 'ñ': 'n', 'č': 'c', 'ć': 'c', 'ğ': 'g', 'ß': 'ss', 'ń': 'n', 'š': 's'}
-        for col in self.columns_to_process:
-            self.df[col] = self.df[col].replace(d, regex=True)
+        for col in columns:
+            df[col] = df[col].replace(d, regex=True)
+        return df
 
-    def delete_punctuation(self):
+    def delete_punctuation(self, df, columns):
         """Elimina signos de puntuación de los textos"""
-        for col in self.columns_to_process:
-            self.df[col] = self.df[col].str.replace(f'[{string.punctuation}]', ' ', regex=True) # self.df[col] = self.df[col].str.replace(f'[{string.punctuation}]', ' ')
+        for col in columns:
+            df[col] = df[col].str.replace(f'[{string.punctuation}]', ' ', regex=True)
+        return df
 
-    def tokenize(self):
+    def tokenize(self, df, columns):
         """Tokeniza los textos"""
-        for col in self.columns_to_process:
-            self.df[col] = self.df[col].str.split()
+        for col in columns:
+            df[col] = df[col].str.split()
+        return df
 
-    def stop_word_removal(self):
+    def stop_word_removal(self, df, columns):
         """Elimina palabras vacías de los textos"""
         url = 'https://raw.githubusercontent.com/7PartidasDigital/AnaText/master/datos/diccionarios/vacias.txt'
         palabras_vacias = pd.read_csv(url, squeeze=True)
-        for col in self.columns_to_process:
-            self.df[col] = self.df[col].apply(lambda x: [word for word in x if word not in palabras_vacias])
+        for col in columns:
+            df[col] = df[col].apply(lambda x: [word for word in x if word not in palabras_vacias])
+        return df
 
     def download_stop_word_removal_file(self):
         """Descarga el archivo de palabras vacías"""
@@ -60,8 +58,9 @@ class TextPreparation:
         r = requests.get(url, allow_redirects=True)
         open('vacias.txt', 'wb').write(r.content)
 
-    def stemming(self):
+    def stemming(self, df, columns):
         """Aplica stemming a los textos"""
         spanish_stemmer = SnowballStemmer('spanish')
-        for col in self.columns_to_process:
-            self.df[col] = self.df[col].apply(lambda x: [spanish_stemmer.stem(word) for word in x])
+        for col in columns:
+            df[col] = df[col].apply(lambda x: [spanish_stemmer.stem(word) for word in x])
+        return df
